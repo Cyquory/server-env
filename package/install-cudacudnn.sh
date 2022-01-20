a19a3ed8d0ce2fe6263426e4cad986d83c792666
Cyquory <recapseason@gmail.com>
2022-01-21 03:22:38 +0800

    fix typo

--- package/install-cudacudnn.sh
+++ package/install-cudacudnn.sh
@@ -31,17 +31,27 @@ install () {
 
   # install cuDNN library
   printf "Installing cuDNN ${CUDNN_VERSION}... "
-  sudo tar --no-same-owner -xJf cudnn.tar.gz -C /usr/local
+  sudo tar --no-same-owner -xJf cudnn.tar.xz -C /usr/local
+  # Copy the following files into the CUDA toolkit directory
+  sudo cp cudnn-*-archive/include/cudnn*.h /usr/local/cuda/include
+  sudo cp -P cudnn-*-archive/lib/libcudnn* /usr/local/cuda/lib64
+  sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
   echo "Done."
 
   # remove default symbloic link
   sudo rm /usr/local/cuda
 
   # clean up
-  rm cuda.run cudnn.tar.gz
+  rm cuda.run cudnn.tar.xz
 }
 
 # install each version of CUDA and cuDNN
+# install "11.3.1" "8.2.1" \
+#   "https://developer.download.nvidia.com/compute/cuda/11.3.1/local_installers/cuda_11.3.1_465.19.01_linux.run" \
+#   "https://developer.download.nvidia.com/compute/redist/cudnn/v8.2.1/cudnn-11.3-linux-x64-v8.2.1.32.tgz"
+
+echo "Installation change after cuda 11.5, proceed with care!"
+echo "This script cannot be apply to older versions"
 install "11.5.1" "8.3.2" \
   "https://developer.download.nvidia.com/compute/cuda/11.5.1/local_installers/cuda_11.5.1_495.29.05_linux.run" \
   "http://developer.download.nvidia.com/compute/redist/cudnn/v8.3.2/local_installers/11.5/cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz"
